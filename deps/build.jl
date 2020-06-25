30c0d32653ba0a95a7567e12714b3a501c53c885
Yao Lu <luyaocns@gmail.com>
2020-06-25 14:58:49 +0800

    remove windows in travis

--- deps/build.jl
+++ deps/build.jl
@@ -4,7 +4,9 @@ ENV["TA_LIBRARY_PATH"] = joinpath(DEPOT_PATH[1], "talib/lib")
 ENV["TA_INCLUDE_PATH"] = joinpath(DEPOT_PATH[1], "talib/include")
 ENV["JULIA_DEPOT_PATH"] = DEPOT_PATH[1]
 
-Sys.iswindows() || run(`bash talib-install.sh`)
 run(`$python -m pip install --upgrade pip`)
 run(`$python -m pip install tsfresh pyarrow chinesecalendar`)
-run(`$python -m pip --no-cache-dir install TA-Lib`)
+if !Sys.iswindows()
+    run(`bash talib-install.sh`)
+    run(`$python -m pip --no-cache-dir install TA-Lib`)
+end
