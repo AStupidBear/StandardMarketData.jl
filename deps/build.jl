f14c39c2703ad395c763c6649a2ba8ba524a986d
Yao Lu <luyaocns@gmail.com>
2020-07-07 01:57:02 +0800

    pip upgrade pandas

--- deps/build.jl
+++ deps/build.jl
@@ -5,7 +5,7 @@ ENV["TA_INCLUDE_PATH"] = joinpath(DEPOT_PATH[1], "talib/include")
 ENV["JULIA_DEPOT_PATH"] = DEPOT_PATH[1]
 
 run(`$python -m pip install --upgrade pip`)
-run(`$python -m pip install tsfresh pyarrow chinesecalendar`)
+run(`$python -m pip install --upgrade "pandas<=0.25.3" tsfresh pyarrow chinesecalendar`)
 if !Sys.iswindows()
     run(`bash talib-install.sh`)
     run(`$python -m pip --no-cache-dir install TA-Lib`)
