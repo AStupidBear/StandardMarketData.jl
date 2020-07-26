f79c60fd01261df25247bcb6ab09d54e9f7ebcc1
Yao Lu <luyaocns@gmail.com>
2020-07-27 00:32:17 +0800

    arm

--- deps/talib-install.sh
+++ deps/talib-install.sh
@@ -2,8 +2,11 @@
 TALIB=$JULIA_DEPOT_PATH/talib
 if [ ! -d $TALIB ]; then
     url=http://prdownloads.sourceforge.net/ta-lib/ta-lib-0.4.0-src.tar.gz
-    cd /tmp/ && wget $url
-    tar xvzf ta-lib-0.4.0-src.tar.gz
-    cd ta-lib && ./configure --prefix=$TALIB
+    cd /tmp/ && wget -O ta-lib.tar.gz $url && tar xvzf ta-lib.tar.gz
+    if [ "$(arch)" == aarch64 ]; then
+        cd ta-lib && ./configure --prefix=$TALIB --build=arm
+    else
+        cd ta-lib && ./configure --prefix=$TALIB
+    fi
     make && make install
 fi
